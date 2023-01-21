0d1de6d67522092b541f7291c6d76978eae6e868
João Vitor Fontoura <jvfontouraj@gmail.com>
2023-01-21 11:49:11 -0300

    update

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const userCardTemplate = document.querySelector("[data-user-template]");
-const userCardContainer = document.querySelector("[data-user-cards-container]")
-const searchInput = document.querySelector("[data-search]")
+const cardTemplate = document.querySelector(".card-template")
+const cardContainer = document.querySelector(".cards-container")
+const searchInput = document.querySelector(".data-search")
 
-let users = []
+let ingredients = []
 
-searchInput.addEventListener("input", el =>{
+searchInput.addEventListener("input", (el)=>{
   const value = el.target.value.toLowerCase()
-  users.forEach(user=>{
-    const isVisible = 
-      user.name.toLowerCase().includes(value) || 
-      user.email.toLowerCase().includes(value)
+  ingredients.forEach(user => {
+    const isVisible = user.name.toLowerCase().includes(value) ||
+    user.why.toLowerCase().includes(value) ||
+    user.updateDate.toLowerCase().includes(value) ||
+    user.font.toLowerCase().includes(value) ||
+    user.itsVegan.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisible)
   })
 })
 
+fetch("../data.json").then(res => res.json()).then(data => {
+  ingredients = data.Ingredientes.map(user => {
+    const card = cardTemplate.content.cloneNode(true).children[0]
+    const header = card.querySelector(".header")
+    const why = card.querySelector(".why")
+    const updateDate = card.querySelector(".update-date")
+    const font = card.querySelector(".font")
+    const itsVegan =card.querySelector(".its-vegan")
 
-fetch("data.json").then(res => res.json()).then(data => {
-  users = data.map(user => {
-    const card = userCardTemplate.content.cloneNode(true).children[0]
-    const header = card.querySelector("[data-header]")
-    const body = card.querySelector("[data-body]")
     header.textContent = user.name
-    body.textContent = user.email
-    userCardContainer.append(card)
-    return {name: user.name, email: user.email, element: card}
+    why.textContent = user.why
+    updateDate.textContent = user.updateDate
+    font.textContent = user.font
+    itsVegan.textContent = user.itsVegan
+
+    cardContainer.append(card)
+
+    return {name: user.name, why: user.why, updateDate: user.updateDate, font: user.font, itsVegan: user.itsVegan, element: card}
   });
 })
