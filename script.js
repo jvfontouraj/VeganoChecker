a706c00972131eae60f45f86693942c6c5dfb49f
João Vitor Fontoura <jvfontouraj@gmail.com>
2023-01-23 07:41:58 -0300

    update

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ selectType.addEventListener('change', function() {
 });
 
 let ingredients = []
+let produtos = []
 
 searchInput.addEventListener("input", (el)=>{
   const value = el.target.value.toLowerCase();
@@ -19,24 +20,27 @@ searchInput.addEventListener("input", (el)=>{
       user.why.toLowerCase().includes(value) ||
       user.updateDate.toLowerCase().includes(value) ||
       user.font.toLowerCase().includes(value) ||
-      user.itsVegan.toLowerCase().includes(value)
+      user.itsVegan.toLowerCase().includes(value)||
+      user.objType.toLowerCase().includes(value)
       user.element.classList.toggle("hide", !isVisible)
     })
   } else{
-produtos.forEach(user => {
+    produtos.forEach(user => {
       const isVisible = user.name.toLowerCase().includes(value) ||
       user.why.toLowerCase().includes(value) ||
-user.brand.toLowerCase().includes(value) ||
-user.animalIngrd.toLowerCase().includes(value) ||
+      user.brand.toLowerCase().includes(value) ||
+      user.animalIngrd.toLowerCase().includes(value) ||
       user.updateDate.toLowerCase().includes(value) ||
       user.font.toLowerCase().includes(value) ||
-      user.itsVegan.toLowerCase().includes(value)
+      user.itsVegan.toLowerCase().includes(value) ||
+      user.objType.toLowerCase().includes(value)
       user.element.classList.toggle("hide", !isVisible)
- } 
+    })
+  }
 })
 
 fetch("../data.json").then(res => res.json()).then(data => {
-  ingredients = data.Ingredientes.map(user => {
+    ingredients = data.Ingredientes.map(user => {
     const card = cardTemplate.content.cloneNode(true).children[0]
     const header = card.querySelector(".header")
     const why = card.querySelector(".why")
@@ -52,6 +56,26 @@ fetch("../data.json").then(res => res.json()).then(data => {
 
     cardContainer.append(card)
 
-    return {name: user.name, why: user.why, updateDate: user.updateDate, font: user.font, itsVegan: user.itsVegan, element: card}
-  });
+    return {objType: user.objType, name: user.name, why: user.why, updateDate: user.updateDate, font: user.font, itsVegan: user.itsVegan, element: card}
+    });
+
+    produtos = data.Produtos.map(user => {
+    const card = cardTemplate.content.cloneNode(true).children[0]
+    const header = card.querySelector(".header")
+    const why = card.querySelector(".why")
+    const updateDate = card.querySelector(".update-date")
+    const font = card.querySelector(".font")
+    const itsVegan =card.querySelector(".its-vegan")
+
+    header.textContent = user.name
+    why.textContent = user.why
+    updateDate.textContent = user.updateDate
+    font.textContent = user.font
+    itsVegan.textContent = user.itsVegan
+
+    cardContainer.append(card)
+
+    return {objType: user.objType, name: user.name, why: user.why, updateDate: user.updateDate, font: user.font, itsVegan: user.itsVegan, element: card}
+    });
+  
 })
