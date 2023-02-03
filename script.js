6112384315d2facd7b3115268671cbc6bd9a3302
João Vitor Fontoura <jvfontouraj@gmail.com>
2023-02-03 11:27:17 -0300

    update

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const undfProdTemplate = document.querySelector(".undf-products-template")
 const ingrCardContainer = document.querySelector(".ingr-cards-container")
 const prodCardContainer = document.querySelector(".prod-cards-container")
 const searchInput = document.querySelector(".data-search")
-const ingrMessage = document.querySelector(".ingr-message")
-const prodMessage = document.querySelector(".prod-message")
 
 let selectType = document.querySelector(".select-type")
 let optionValue = "Ingredientes"
@@ -37,7 +35,7 @@ selectType.addEventListener('change', function() {
 
 searchInput.addEventListener("input", (el)=>{
   const value = el.target.value.toLowerCase();
-
+  
   if(optionValue === "Ingredientes"){
     ingredients.forEach(user => {
       const isVisible = user.name.toLowerCase().includes(value) ||
@@ -46,7 +44,7 @@ searchInput.addEventListener("input", (el)=>{
       user.font.toLowerCase().includes(value) ||
       user.itsVegan.toLowerCase().includes(value)||
       user.objType.toLowerCase().includes(value)
-      user.element.classList.toggle("hide", !isVisible || (value === "vegano" && !user.veganCheck))
+      user.element.classList.toggle("hide", !isVisible)
     })
 
   } else{
@@ -75,21 +73,21 @@ fetch("../data.json").then(res => res.json()).then(data => {
     card = undfIngrTemplate.content.cloneNode(true).children[0]
   }
   const header = card.querySelector(".header")
+  const itsVegan =card.querySelector(".its-vegan")
   const why = card.querySelector(".why")
   const updateDate = card.querySelector(".update-date")
   const font = card.querySelector(".font")
-  const itsVegan =card.querySelector(".its-vegan")
 
   header.textContent = user.name
+  itsVegan.textContent = user.itsVegan
   why.textContent = user.why
   updateDate.textContent = user.updateDate
   font.textContent = user.font
-  itsVegan.textContent = user.itsVegan
 
   font.setAttribute('href', user.fontLink)
   ingrCardContainer.append(card)
 
-  return {objType: user.objType, name: user.name, why: user.why, updateDate: user.updateDate, font: user.font, itsVegan: user.itsVegan, veganCheck: user.veganCheck, element: card}
+  return {objType: user.objType, name: user.name, itsVegan: user.itsVegan, why: user.why, updateDate: user.updateDate, font: user.font, veganCheck: user.veganCheck, element: card}
   });
 
   produtos = data.Produtos.map(user => {
@@ -102,24 +100,24 @@ fetch("../data.json").then(res => res.json()).then(data => {
     card = undfProdTemplate.content.cloneNode(true).children[0]
   }
   const header = card.querySelector(".header")
+  const itsVegan =card.querySelector(".its-vegan")
   const brand = card.querySelector(".brand")
   const why = card.querySelector(".why")
   const animalIngr = card.querySelector(".animal-ingr")
   const updateDate = card.querySelector(".update-date")
   const font = card.querySelector(".font")
-  const itsVegan =card.querySelector(".its-vegan")
 
   header.textContent = user.name
+  itsVegan.textContent = user.itsVegan
   brand.textContent = user.brand
   why.textContent = user.why
   animalIngr.textContent = user.animalIngr
   updateDate.textContent = user.updateDate
   font.textContent = user.font
-  itsVegan.textContent = user.itsVegan
 
   font.setAttribute('href', user.fontLink)
   prodCardContainer.append(card)
 
-  return {objType: user.objType, name: user.name, brand: user.brand, why: user.why, animalIngr: user.animalIngr, updateDate: user.updateDate, font: user.font, itsVegan: user.itsVegan, veganCheck: user.veganCheck, element: card}
+  return {objType: user.objType, name: user.name, itsVegan: user.itsVegan, brand: user.brand, why: user.why, animalIngr: user.animalIngr, updateDate: user.updateDate, font: user.font, veganCheck: user.veganCheck, element: card}
   });
 })
